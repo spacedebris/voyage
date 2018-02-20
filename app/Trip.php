1f42bedc24c50ae537c5cbba13014475063809ab
spacedebris <kozlowskimarekamil@gmail.com>
2018-02-20 17:09:53 +0100

    basic auth debug bar acl

---
+++ app/Trip.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Trip extends Model
+{
+
+
+    public $fillable = ['title','description'];
+
+
+}
---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+ 
+@section('content')
+	<h1>Nie masz uprawnień do wyświetlenia strony.</h1>
+@endsection
